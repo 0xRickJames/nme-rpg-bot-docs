af2f7822df690e486c9798a8ca2404167a3aa04a
Rick James McFarley <98493254+0xRickJames@users.noreply.github.com>
2025-02-07 13:29:21 -0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+  import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import { useRouter } from "next/router";
 
@@ -9,11 +9,11 @@ const config: DocsThemeConfig = {
     if (asPath !== "/") {
       return {
         titleTemplate: "%s – NME Bot",
-        description: "NME Bot is your trusted assistant for managing communities and interacting with the Sonic blockchain.",
       };
     }
   },
 
+  description: "NME Bot is your trusted assistant for managing communities and interacting with the Sonic blockchain.",
   faviconGlyph: "⚔",
   logo: <span>NME Bot</span>,
   gitTimestamp: null,
